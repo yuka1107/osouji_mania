b5fd4878d7e453562e838e8bb8c33ab8db7f5b4c
EC2 Default User <ec2-user@ip-172-31-27-127.ec2.internal>
2021-12-30 15:56:05 +0000

    [Add]top,aboutページ作成(中身記述なし)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,22 @@
 Rails.application.routes.draw do
 
-  devise_for :admins
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :admins, controllers: {
+    sessions: 'admins/sessions'
+  }
+
+  devise_for :users, controllers: {
+    rgistrations: 'users/registrations',
+    sessions: 'users/sessions',
+    destroy: 'users/sessions/destroy'
+  }
+
+  namespace :admin do
+    root to: 'admin/homes#top'
+  end
+
+  scope module: :public do
+    root to: 'homes#top'
+    get 'about' => 'homes#about'
+  end
+
 end
