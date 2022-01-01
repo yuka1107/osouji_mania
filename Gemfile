2d28266cf2b4815a48ab8ab8364656d891bc59dc
EC2 Default User <ec2-user@ip-172-31-27-127.ec2.internal>
2022-01-01 13:55:41 +0000

    [Add]ヘッダー・フッター作成

--- Gemfile
+++ Gemfile
@@ -66,3 +66,4 @@ gem 'jquery-rails'
 gem 'font-awesome-sass', '~> 5.13'
 
 gem 'devise'
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  attachment :profile_image
+  
 end
