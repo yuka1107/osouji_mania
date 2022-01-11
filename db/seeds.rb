6c932c40b5211914b4f0b47a5762a6e6e082ad0c
EC2 Default User <ec2-user@ip-172-31-27-127.ec2.internal>
2022-01-11 08:05:08 +0000

    seedに初期ログインを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# adminの初期ログインメールとパスワード
+Admin.create!(
+  email: 'yukauchiumi1995@gmail.com',
+  password: '123456'
+)
