830747fb123ace5b439d36d067679ff710c650e5
EC2 Default User <ec2-user@ip-172-31-27-127.ec2.internal>
2022-01-12 05:50:15 +0000

    お問い合わせ機能実装(Gmaik二段階認証の設定未)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,6 +32,16 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
+  config.avtion_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    user_name: '<yukauchiumi1995@gmail.com>',
+    password: '<yoinotuki16>',
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 
   config.action_mailer.perform_caching = false
 
